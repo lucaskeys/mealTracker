0ce4d8ab270b4f1798c2c6060cf41776348d6936
lk <lucaskeys@gmail.com>
2017-01-19 19:39:55 -0800

    Add model and create meal list in appComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
-
+import { Meal } from './meal.model';
 
 @Component({
     selector: 'app-root',
     template: `
     <div class="container">
-      <h1>My First Angular 2 App</h1>
+      <h1>Oops I ate it again!</h1>
     </div>
   `
 })
 
 export class AppComponent {
+  currentMeal = null;
 
+  myMealList: Meal[] = [
+    new Meal("Pizza", "I couldnt help myself", 1000),
+    new Meal("Vegetables", "I am eating the food my food eats", 50),
+    new Meal("Tilapia", "Finally a healthy choice I like", 240),
+  ];
+
+  editLog(currentLog) {
+    this.currentMeal = currentLog;
+  }
 }
